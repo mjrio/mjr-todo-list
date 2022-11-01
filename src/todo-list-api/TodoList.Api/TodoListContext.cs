4ba31fc39fdaacf38d57d0318c4cde112c625d85
Stijn Geens <33414262+degeens@users.noreply.github.com>
2022-11-01 09:01:28 +0100

    Fix typo

--- src/todo-list-api/TodoList.Api/TodoListContext.cs
+++ src/todo-list-api/TodoList.Api/TodoListContext.cs
@@ -4,7 +4,7 @@ public class TodoListContext : DbContext
 {
     public TodoListContext(DbContextOptions<TodoListContext> options) : base(options)
     {
-        Database.EnsureCreatedAsync();
+        Database.EnsureCreated();
     }
 
     public DbSet<Todo> Todos { get; set; }
